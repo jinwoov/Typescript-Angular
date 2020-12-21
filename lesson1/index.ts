f68a22373c663959cc721b051dac82036b2338cb
Jin Kim <jinwoov@gmail.com>
2020-12-21 00:02:02 -0600

    learning

---
+++ lesson1/index.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+let firstvalue = document.getElementById("first-value")! as HTMLInputElement;
+let secondvalue = document.getElementById("second-value")! as HTMLInputElement;
+
+// adding the + sign infornt of the variable change the data type to interger
+function addNum(num1: number, num2: number){
+    return +num1 + +num2;
+}
+
+let butt = document.getElementById("add-button");
+
+butt.addEventListener("click", function (e) {
+    console.log(addNum(firstvalue.value, secondvalue.value));
+    e.preventDefault();
+})
