46d8a7e49d3ad0d75d24265466ceea046ff99e9b
Jin Kim <jinwoov@gmail.com>
2021-02-03 08:58:33 -0600

    working process

--- angular/intro2angular/src/app/app.module.ts
+++ angular/intro2angular/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/third-lecture/src/app/app.component.ts
+++ angular/third-lecture/src/app/app.component.ts
@@ -3,12 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.sass']
-  styles: [`
-    h3 {
-      color: red;
-    }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'third-lecture';
 }
